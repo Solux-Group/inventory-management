239512e09fee652b0988371aa8f1d4dc2e8182bd
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-13 15:46:39 +0000

    reset auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,13 @@ const { SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(403).json({
-      status: 403,
-      message: "Token authorization is required",
-      error: true,
-    });
-  }
+  // if (!token) {
+  //   return res.status(403).json({
+  //     status: 403,
+  //     message: "Token authorization is required",
+  //     error: true,
+  //   });
+  // }
 
   try {
     const decoded = jwt.verify(token, SECRET);
