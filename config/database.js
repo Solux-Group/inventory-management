7eae4573abc2655828db1e41e0c93a7cb52a00e2
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-13 15:19:50 +0000

    Update database.js
    Change old database link

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const { MONGODB_URI } = process.env;
+//const { MONGODB_URI } = process.env || "mongodb://127.0.0.1:27017/db_inventory";
 
 exports.connect = () => {
-  mongoose.connect(MONGODB_URI || "mongodb://127.0.0.1:27017/db_inventory", {
+  mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
