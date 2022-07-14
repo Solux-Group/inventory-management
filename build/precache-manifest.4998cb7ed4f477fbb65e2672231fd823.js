0fddafa24670c08c149a75c371d48c80b64b07d2
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-14 08:09:21 +0000

    showroom deploy

---
+++ build/precache-manifest.4998cb7ed4f477fbb65e2672231fd823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14ef659e2d0dea540c39",
+    "url": "/inventory-management/static/css/main.1f31640a.chunk.css"
+  },
+  {
+    "revision": "14ef659e2d0dea540c39",
+    "url": "/inventory-management/static/js/main.623b3b0d.chunk.js"
+  },
+  {
+    "revision": "665e70f04a797e42d703",
+    "url": "/inventory-management/static/js/runtime~main.31ad1e1e.js"
+  },
+  {
+    "revision": "d2dbf72b7c97490a69ba",
+    "url": "/inventory-management/static/js/2.b64c1060.chunk.js"
+  },
+  {
+    "revision": "d4ef7331cee51ceb592cee42b2d0455f",
+    "url": "/inventory-management/index.html"
+  }
+];
