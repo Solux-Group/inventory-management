279ed52da013c8803088c34264bf6b5bc0d707f1
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-25 15:42:04 +0000

    test again for stok

--- controllers/barangKeluarController.js
+++ controllers/barangKeluarController.js
@@ -231,8 +231,8 @@ module.exports = {
     try {
       await barangModel.findOneAndUpdate(
         { kode_barang: kode_barang },
+        { $inc: { stok: -kuantitas } },
         { $inc: id_showroom === "62cfd0a4f824a84be4da0065" ? { stok1: -kuantitas } : id_showroom === "62ceeff20fe57200df0243a5" ? { stok2: -kuantitas } : ""},
-        { stok: -kuantitas },
       );
 
       const newBarangKeluar = new barangKeluarModel({
