2962bd2325e77cfe142bd00fe4994f2c581b30b0
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-25 13:00:55 +0000

    update api endpoint stage

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // export const ENDPOINT = "https://inventory-mern-app.herokuapp.com";
 // export const ENDPOINT = process.env.API_ENDPOINT || "http://localhost:5000";
-export const ENDPOINT = process.env.API_ENDPOINT_STAGE || "https://inventory-management-stage.herokuapp.com/";
+export const ENDPOINT = process.env.API_ENDPOINT_STAGE || "https://inventory-management-api-stage.herokuapp.com/";
 
 export default axios.create({
   baseURL: ENDPOINT,
