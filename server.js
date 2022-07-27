a99b8db5716036406c3d9d376eead579d1e162aa
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-27 11:42:53 +0000

    add log

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(cors());
-// app.use(logger("dev")); // log
+app.use(logger("dev")); // log
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
