4321c0ef05634e9afe3a386853cf5acb508069a9
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-27 11:38:34 +0000

    little typo for the MIME type

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(express.static(path.join(__dirname, "public")));
-app.use('public', express.static('public'));
+app.use('/public', express.static('public'));
 
 // auth without middleware
 app.use("/", indexRouter);
