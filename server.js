8fdd06db56e9ac37f4761e384c7b0d0267acabfa
Serigne Mbacke KA <94413584+softlockin@users.noreply.github.com>
2022-07-27 11:40:39 +0000

    Trying to fix MIME Type error

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 // app.use(logger("dev")); // log
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // auth without middleware
 app.use("/", indexRouter);
